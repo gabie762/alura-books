8c2415445b71e41ab67be38c820058826227757d
gabie762 <gabihenriques821@gmail.com>
2024-01-26 17:59:14 -0300

    component pesquisa adicionado

---
+++ src/componentes/Pesquisa/dadosPesquisa.js
@@ -0,0 +1,13 @@
+import livro from '../../imagens/livro.png'
+import livro2 from '../../imagens/livro2.png'
+import livro3 from '../../imagens/livro3.jpg'
+import livro4 from '../../imagens/livro4.jpg'
+import livro5 from '../../imagens/livro5.jpg'
+
+export const livros = [
+    {nome: 'Liderança em Design', id: 1, src:livro},
+    {nome: 'JavaScript Assertivo', id: 2, src:livro2},
+    {nome: 'Percy Jackson: O Ladrão de Raios', id: 3, src:livro3},
+    {nome: 'Viúva de Ferro', id: 4, src:livro4},
+    {nome: 'Lendários', id: 5, src:livro5}
+]
